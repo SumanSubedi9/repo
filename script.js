3714274c91dcf05f5d61b636a37d70d789de8acf
Suman  Subedi <ssubedi60@gmail.com>
2024-08-30 20:50:24 -0500

    Implemented geo location

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+  },
+  function () {
+    alert("Could not get your position");
+  }
+);
