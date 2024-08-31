2c56cb2dd998f4c67d5d263a0f6d2a6026ecbaf8
Suman  Subedi <ssubedi60@gmail.com>
2024-08-31 15:19:46 -0500

    Implemented library

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 "use strict";
 
+const smallScreen = window.matchMedia("(max-width: 544px)");
+
 navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
+    const map = L.map("map").setView([latitude, longitude], 13);
+
+    L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([latitude, longitude])
+      .addTo(map)
+      .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+      .openPopup();
   },
+
   function () {
     alert("Could not get your position");
   }
