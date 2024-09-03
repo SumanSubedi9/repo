e9c5eaee098ff76dadc4aae0b7f52c55d2e12e9e
Suman  Subedi <ssubedi60@gmail.com>
2024-09-02 21:15:44 -0500

    Worked on displaying the form only when the user clicks on the map / inlcuded hidden class for the form

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 "use strict";
 
-const smallScreen = window.matchMedia("(max-width: 544px)");
+const form = document.querySelector(".form");
+
+const inputName = document.querySelector(".form__input--name");
+const inputType = document.querySelector(".form__input--type");
+const inputDescription = document.querySelector(".form__input--description");
+const inputDate = document.querySelector(".form__input--date");
+
+let map, mapEvent;
 
 navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
-    const map = L.map("map").setView([latitude, longitude], 13);
+    map = L.map("map").setView([latitude, longitude], 13);
 
     L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
       attribution:
@@ -15,22 +22,10 @@ navigator.geolocation.getCurrentPosition(
 
     // Event for when you click on the map
 
-    map.on("click", function (mapEvent) {
-      console.log(mapEvent);
-      const { lat, lng } = mapEvent.latlng;
-      L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: "visited-popup",
-          })
-        )
-        .setPopupContent("Visited")
-        .openPopup();
+    map.on("click", function (mapE) {
+      mapEvent = mapE;
+      form.classList.remove("hidden");
+      inputName.focus(); // Focuses on input Name while loading the page
     });
   },
 
@@ -38,3 +33,27 @@ navigator.geolocation.getCurrentPosition(
     alert("Could not get your position");
   }
 );
+
+// Event for when you submit the form
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // Clear input fields
+  inputName.value = inputDescription.value = inputDate.value = "";
+
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "visited-popup",
+      })
+    )
+    .setPopupContent("Visited")
+    .openPopup();
+});
